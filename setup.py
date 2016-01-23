38407618120d1cac95147592f2fa477f5d1e3db2
Syrus Akbary <me@syrusakbary.com>
2016-01-23 13:11:10 -0800

    Updated version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ required_packages = ['graphql-core>=0.4.9', 'flask>=0.7.0']
 
 setup(
     name='graphql-flask',
-    version='1.0.0',
+    version='1.1.0',
     description='Adds GraphQL support to your Flask application',
     long_description=open('README.rst').read(),
     url='https://github.com/graphql-python/graphql-flask',
