981444673b62bc7d34122d023c7ad5a6bf60655e
davidroeca <david.roeca@gmail.com>
2018-07-05 17:33:08 -0400

    mild refactoring

--- flask_graphql/utils.py
+++ flask_graphql/utils.py
@@ -1,13 +1,10 @@
-from copy import copy
-
-
 def place_files_in_operations(operations, files_map, files):
     paths_to_key = (
         (value.split('.'), key)
         for key, values in files_map.items()
         for value in values
     )
-    output = {}
+    output = new_merged_dict(operations)
     output.update(operations)
     for path, key in paths_to_key:
         file_obj = files[key]
@@ -21,22 +18,24 @@ def add_file_to_operations(operations, file_obj, path):
     if isinstance(operations, dict):
         key = path[0]
         sub_dict = add_file_to_operations(operations[key], file_obj, path[1:])
-        return merge_dicts(
-            operations,
-            {key: sub_dict},
-        )
+        return new_merged_dict(operations, {key: sub_dict})
     if isinstance(operations, list):
         index = int(path[0])
         sub_item = add_file_to_operations(operations[index], file_obj, path[1:])
-        return operations[:index] + [sub_item] + operations[index+1:]
+        return new_list_with_replaced_item(operations, index, sub_item)
     return TypeError('Operations must be a JSON data structure')
 
 
-def merge_dicts(*dicts):
+def new_merged_dict(*dicts):
     # Necessary for python2 support
-    if not dicts:
-        return {}
-    output = copy(dicts[0])
-    for d in dicts[1:]:
+    output = {}
+    for d in dicts:
         output.update(d)
+    return output
+
+
+def new_list_with_replaced_item(input_list, index, new_value):
+    # Necessary for python2 support
+    output = [i for i in input_list]
+    output[index] = new_value
     return output
